1ec4f4615050a98bfea775dd607ea52b5348f5bb
Simone Rubino <daemo00@gmail.com>
2024-07-25 00:36:32 +0200

    Tried suggested plugins

--- init.lua
+++ init.lua
@@ -938,12 +938,12 @@ require('lazy').setup({
   --  Here are some example plugins that I've included in the Kickstart repository.
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
-  -- require 'kickstart.plugins.debug',
-  -- require 'kickstart.plugins.indent_line',
-  -- require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
-  -- require 'kickstart.plugins.neo-tree',
-  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  require 'kickstart.plugins.debug',
+  require 'kickstart.plugins.indent_line',
+  require 'kickstart.plugins.lint',
+  require 'kickstart.plugins.autopairs',
+  -- require 'kickstart.plugins.neo-tree', Tried it, nvim-tree is better to me
+  require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   'sindrets/diffview.nvim', -- Diff view
   'tpope/vim-fugitive', -- Git integration
