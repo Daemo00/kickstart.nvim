d09b2708dff5ec26ef9c8c8888b020ac77a1b841
Simone Rubino <daemo00@gmail.com>
2024-08-19 21:23:37 +0200

    Refactoring tools

--- init.lua
+++ init.lua
@@ -733,6 +733,18 @@ require('lazy').setup({
     },
   },
 
+  { -- Reformat
+    {
+      'ThePrimeagen/refactoring.nvim',
+      dependencies = {
+        'nvim-lua/plenary.nvim',
+        'nvim-treesitter/nvim-treesitter',
+      },
+      config = function()
+        require('refactoring').setup()
+      end,
+    },
+  },
   { -- Autocompletion
     'hrsh7th/nvim-cmp',
     event = 'InsertEnter',
