02bba1d432f039f58ce0ffa834a102fc16df62f8
Simone Rubino <daemo00@gmail.com>
2025-04-08 23:33:13 +0200

    Smoothly resize windows

--- init.lua
+++ init.lua
@@ -208,6 +208,10 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+vim.keymap.set('n', '<Esc>>', ':vertical res +1<Enter>', { desc = 'Increase width smoothly' })
+vim.keymap.set('n', '<Esc><', ':vertical res -1<Enter>', { desc = 'Decrease width smoothly' })
+vim.keymap.set('n', '<Esc>+', ':resize +1<Enter>', { desc = 'Increase height smoothly' })
+vim.keymap.set('n', '<Esc>-', ':resize -1<Enter>', { desc = 'Decrease height smoothly' })
 -- NOTE: Some terminals have coliding keymaps or are not able to send distinct keycodes
 -- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
 -- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
